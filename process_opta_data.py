802c7346b790a17c011783d05c671461f575f7df
SUAOL <salou.sylla@gmail.com>
2026-04-19 01:26:39 +0200

    fix: restauration de import os pour la lecture sécurisée du .env

--- process_opta_data.py
+++ process_opta_data.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import datetime
 import math
+import os
 import json
 from typing import List, Dict, Any, Optional
 from sqlalchemy import create_engine, text
